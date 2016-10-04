7ccf145c71a5489ad75f11b358fba84fa3d8101c
Dimannich <dim199616@mail.ru>
2016-10-04 20:23:29 +1000

    еще раз

---
+++ RoyalPlayingGame/RoyalPlayingGame/Potion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalPlayingGame
+{
+    public enum PotionType { MP, HP}
+    public class Potion:Item
+    { 
+        public Potion(string name, Effect effect, uint potionLvl, PotionType PType, ushort amount, ushort maxAmount):base( name, maxAmount, amount, potionLvl, effect)
+        {
+            this.PType = PType;
+        }
+        public Potion(string name, Effect effect, uint potionLvl, PotionType PType, ushort amount) : base(name, 10, amount, potionLvl, effect)
+        {
+            this.PType = PType;
+        }
+        public void AddPotion()
+        {
+            
+        }
+        public PotionType PType { get; protected set; }
+
+    }
+}
---
+++ RoyalPlayingGame/VisualPart/MenuPainter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VisualPart
+{
+    public class MenuPainter
+    {
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public static void DrawMenu(PaintEventArgs e)
+        {
+
+            //e.Graphics.FillRectangle(Brushes.Black,)
+            
+        }
+    }
+}
