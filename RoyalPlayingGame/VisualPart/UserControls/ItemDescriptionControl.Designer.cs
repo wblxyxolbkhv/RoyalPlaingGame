a1ef366802cab9b267950fb3aad41fdeae70b3dd
Dimannich <dim199616@mail.ru>
2017-02-06 18:56:36 +1000

    merge

--- RoyalPlayingGame/VisualPart/UserControls/ItemDescriptionControl.Designer.cs
+++ RoyalPlayingGame/VisualPart/UserControls/ItemDescriptionControl.Designer.cs
@@ -46,9 +46,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(3, 32);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.Size = new System.Drawing.Size(60, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Описание 123";
+            this.label2.Text = "Описание ";
             // 
             // ItemDescriptionControl
             // 
